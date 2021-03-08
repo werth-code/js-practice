7cdc831447f00724698e6623712f9b1cb64e78d0
Matt Werth <matthewwerth@gmail.com>
2021-03-08 09:54:35 -0500

    Added Stack With Node

---
+++ algorithms/stack.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null
+        this.bottom = null
+        this.length = 0
+    }
+
+    pop() {
+
+    }
+
+    push() {
+        
+    }
+
+    peek() {
+
+    }
+}
