8258cfce4b03c3db26c998554cc37e200863c8d8
Matt Werth <matthewwerth@gmail.com>
2021-03-08 11:56:50 -0500

    Added Trees

---
+++ algorithms/trees.js
@@ -0,0 +1,17 @@
+class Node {
+    constructor(data) {
+       this.data = data
+       this.left = null
+       this.right = null
+    }
+    
+}
+class Trees {
+    constructor() {
+        this.root = null
+        this.left = null
+        this.right = null
+        this.height = 0
+    }
+    
+}
